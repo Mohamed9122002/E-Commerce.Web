561f6814436adb52bc3582ba8835e6f6b633ddcb
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-04-14 23:42:55 +0200

    Product Module

---
+++ Core/DomainLayer/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Product :BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        // Navigation properties 
+        public ProductBrand ProductBrand { get; set; }
+        public int BrandId { get; set; } // Freign key
+        public ProductType ProductType { get; set; }
+        public int TypeId { get; set; } // Freign key
+
+
+    }
+}
