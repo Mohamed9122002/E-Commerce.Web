0d1c65c60d506b64df666e37c4b1a32a03d0c0cc
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-04-26 00:11:55 +0300

    Basket Module Repository

---
+++ Core/DomainLayer/Contracts/IBasketRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer.Models.BasketModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket?> GetBasketAsync(string key);
+        Task<CustomerBasket> CreateORUpdateBasketAsync(CustomerBasket basket, TimeSpan? TimeLive = null);
+        Task<bool> DeleteBasketAsync(string key);
+
+
+    }
+}
--- Infrastructure/Persistence/InfrastructureServicesRegistration.cs
+++ Infrastructure/Persistence/InfrastructureServicesRegistration.cs
@@ -1,10 +1,11 @@
 ﻿
 
 using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace Persistence
 {
-  public static  class InfrastructureServicesRegistration
+    public static class InfrastructureServicesRegistration
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection Services, IConfiguration Configuration)
         {
@@ -14,8 +15,13 @@ namespace Persistence
             });
             Services.AddScoped<IDataSeeding, DataSeeding>();
             Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+            Services.AddScoped<IBasketRepository, BasketRepository>();
+            Services.AddSingleton<IConnectionMultiplexer>((_) =>
+            {
+                return  ConnectionMultiplexer.Connect(Configuration.GetConnectionString("RedisConnectionString"));
+            });
             return Services;
         }
+
     }
 }
