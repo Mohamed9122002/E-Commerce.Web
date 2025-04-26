87fedb01856a453167d193fe8744d5e066b9dab3
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-04-26 16:07:12 +0300

    BasketModuleController

---
+++ Infrastructure/Presentation/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObject.BasketModuleDTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController(IServiceManager _serviceManager) : ControllerBase
+    {
+        // Get Baket 
+        [HttpGet] // get/BaseUrl/api/Basket
+        public async Task<ActionResult<BasketDTo>> GetBasket(string key)
+        {
+            var Basket = await _serviceManager.BasketServices.GetBasketAsync(key);
+            return Ok(Basket);
+        }
+        // Create Basket or Updated 
+        [HttpPost]
+        public async Task<ActionResult<BasketDTo>> CreateOrUpdateBasket( BasketDTo basket)
+        {
+            var Basket = await _serviceManager.BasketServices.CreateORUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+        // Delete Basket 
+        [HttpDelete("{key}")] //Delete/BaseUrl/api/Basket/{key}
+        public async Task<ActionResult<bool>> DeleteBasket(string key)
+        {
+            var result = await _serviceManager.BasketServices.DeleteBasketAsync(key);
+                return Ok(result);
+      
+        }
+    }
+}
