9a42d4a89f2a844feff05318cc0ab9b4120e8c32
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-04-26 15:49:20 +0300

    Basket Module - Service

--- Core/DomainLayer/Models/BasketModule/BasketItem.cs
+++ Core/DomainLayer/Models/BasketModule/BasketItem.cs
@@ -9,8 +9,8 @@ namespace DomainLayer.Models.BasketModule
   public  class BasketItem
     {
         public int  Id { get; set; }
-        public string ProductName { get; set; } = null!;
-        public string PictureUrl { get; set; } = null!;
+        public string ProductName { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
         public decimal Price { get; set; }
         public int Quantity { get; set; }
 
