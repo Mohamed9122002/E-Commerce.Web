6f31d1a8c2f10d7294ba837472607d581e2cef99
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-05 19:27:35 +0300

    Order Module Entities

---
+++ Core/DomainLayer/Models/OrderModule/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModule
+{
+    public class Order :BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public  DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public OrderAddress OrderAddress { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public int DeliveryMethodId { get; set; } = default!; // Fk 
+
+        public OrderStatus OrderStatus { get; set; } 
+
+        public ICollection<OrderItem> OrderItems { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        
+        public decimal GetTotal()
+        {
+            return SubTotal + DeliveryMethod.Price;
+        }
+    }
+}
---
+++ Core/DomainLayer/Models/OrderModule/ProductItemOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModule
+{
+   public class ProductItemOrder
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = default!;
+
+         public string ProductUrl{ get; set; } = default!;
+
+    }
+}
