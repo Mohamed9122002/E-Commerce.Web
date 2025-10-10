5b3b4888c27239880f22c77e27142e0c6029f6aa
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-10 16:50:32 +0300

    Basket Module - Entities

---
+++ Core/DomainLayer/Models/BaskedModule/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.BaskedModule
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string PictureURL { get; set; } = default!;
+        public decimal Price { get; set; } 
+        public int Quantity { get; set; } 
+
+
+    }
+}
---
+++ Core/DomainLayer/Models/BaskedModule/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.BaskedModule
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } // Guid
+        public ICollection<BasketItem> BasketItems { get; set; } = [];
+     }
+}
