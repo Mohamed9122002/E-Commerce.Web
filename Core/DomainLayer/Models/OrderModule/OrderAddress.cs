837ebaf83363f2041b96ac5efdf29e485c608275
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-11 21:40:45 +0300

    Order Module Entities

---
+++ Core/DomainLayer/Models/OrderModule/OrderAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModule
+{
+    public class OrderAddress
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string City { get; set; } = default!;
+    }
+}
---
+++ Core/DomainLayer/Models/OrderModule/ProductItemOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModule
+{
+    public class ProductItemOrder
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string PictureURL { get; set; } = default!; 
+    }
+}
