cadc398acc0212e158d9e021b67dbd9fc23a1c85
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-11 15:51:44 +0300

    AuthenticationController [Login & Register]

---
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DTOS.AuthDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AuthenticationController(IServiceManager _serviceManager) : ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var User = await _serviceManager.AuthenticationService.LoginAsync(loginDto);
+            return Ok(User);
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var User = await _serviceManager.AuthenticationService.RegisterAsync(registerDto);
+            return Ok(User);
+        }
+
+    }
+}
