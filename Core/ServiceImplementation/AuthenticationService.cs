6faa8205702c843bdb852c2cdc3f7d472c9a3808
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-11 16:25:41 +0300

    CreateToke

--- Core/ServiceImplementation/AuthenticationService.cs
+++ Core/ServiceImplementation/AuthenticationService.cs
@@ -1,17 +1,21 @@
 ﻿using DomainLayer.Exceptions;
 using DomainLayer.Models.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using ServiceAbstraction;
 using Shared.DTOS.AuthDTOs;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceImplementation
 {
-    public class AuthenticationService(UserManager<ApplicationUser> _userManager) : IAuthenticationService
+    public class AuthenticationService(UserManager<ApplicationUser> _userManager, IConfiguration _configuration) : IAuthenticationService
     {
         public async Task<UserDto> LoginAsync(LoginDto loginDto)
         {
@@ -26,7 +30,7 @@ namespace ServiceImplementation
                 {
                     DisplayName = User.DisplayName,
                     Email = User.Email,
-                    Token = CreateTokenAsync(User),
+                    Token = await CreateTokenAsync(User),
                 };
             }
             else
@@ -54,7 +58,7 @@ namespace ServiceImplementation
                 {
                     DisplayName = User.DisplayName,
                     Email = User.Email,
-                    Token = CreateTokenAsync(User),
+                    Token = await CreateTokenAsync(User),
                 };
 
             }
@@ -66,9 +70,30 @@ namespace ServiceImplementation
             }
         }
 
-        private static string CreateTokenAsync(ApplicationUser user)
+        private async Task<string> CreateTokenAsync(ApplicationUser user)
         {
-            return "ToDo";
+            var Claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email,user.Email!),
+                new Claim(ClaimTypes.Name,user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier,user.Id!),
+            };
+            var Roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in Roles)
+            {
+                Claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var SecretKey = _configuration.GetSection("JWTOptions")["SecretKey"];
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var Credentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+            // Create Token 
+            var Token = new JwtSecurityToken(
+                 issuer: _configuration["JWTOptions:Issuer"],
+                audience: _configuration["JWTOptions:Audience"],
+               claims: Claims,
+               expires: DateTime.Now.AddHours(1),
+               signingCredentials: Credentials);
+            return new JwtSecurityTokenHandler().WriteToken(Token);
         }
     }
 }
