f0552cb6da86b0f4f44cd889cfa12e29301cfad6
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-12 04:29:28 +0300

    Order Controller Get All Delivery Methods - Get All Orders - Get Order By Id

--- Infrastructure/Presentation/Controllers/OrdersController.cs
+++ Infrastructure/Presentation/Controllers/OrdersController.cs
@@ -21,6 +21,25 @@ namespace Presentation.Controllers
             var Order = await _serviceManager.OrderService.CreateOrder(orderDto, GetEmailFromToken());
             return Ok(Order);
         }
+        // GetAllOrder 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTo>>> GetAllOrders()
+        {
+            var Orders = await _serviceManager.OrderService.GetAllOrdersAsync(GetEmailFromToken());
+            return Ok(Orders);
+        }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<OrderToReturnDTo>> GetOrderById(Guid Id)
+        {
+            var Order = await _serviceManager.OrderService.GetAllOrderById(Id);
+            return Ok(Order);
+        }
+        [HttpGet("DeliveryMethods")]
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDTO>>> GetAllDeliveryMethod()
+        {
+            var deliveryMethods = await _serviceManager.OrderService.GetDeliveryMethodsAsync();
+            return Ok(deliveryMethods);
+        }
     }
 }
 
